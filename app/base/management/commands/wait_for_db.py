9bb800ee2eadd8dc537e709abaee98989a4f6530
Revelvol <111274957+Revelvol@users.noreply.github.com>
2023-02-04 22:17:40 +0700

    fixed wait for database dready

--- app/base/management/commands/wait_for_db.py
+++ app/base/management/commands/wait_for_db.py
@@ -2,8 +2,7 @@ from django.core.management.base import BaseCommand
 import time
 from django.db import connection
 from django.db.utils import OperationalError
-
-
+from psycopg2 import OperationalError as Psycop2OpError
 
 class Command(BaseCommand):
     help = "Wait for postgres database to be ready "
@@ -13,11 +12,10 @@ class Command(BaseCommand):
         db_conn = False
         while not db_conn:
             try:
-                connection.ensure_connection()
+                self.check(databases=['default'])
                 db_conn = True
-            except OperationalError:
+            except (OperationalError, Psycop2OpError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
-
         self.stdout.write(self.style.SUCCESS('Database available!'))
 
